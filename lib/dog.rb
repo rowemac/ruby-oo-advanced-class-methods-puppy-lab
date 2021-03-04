f8eaeef2daaa678848dc99725ddfcac9f3767a8b
Mac Rowe <m.rowe42@gmail.com>
2021-03-04 10:10:24 -0500

    commit tree 59b93c8d5569326fd253c288a8929ba2050ab721

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,37 @@
+require 'pry'
+
 # Add your code here
+class Dog
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear 
+    end
+
+    
+   def self.print_all
+        @@all.each do | dog |
+            puts dog.name
+        end 
+    end
+
+    def save
+        @@all << self
+    end
+
+    def name
+        @name
+    end
+
+end 
+
